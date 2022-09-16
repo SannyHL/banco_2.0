6a51c538ed6f0d72d42569578610c76a7acb63ad
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-16 16:49:17 -0300

    criação métodos

--- bancosan/src/main/java/com/banco/bancosan/services/TransacoesContasService.java
+++ bancosan/src/main/java/com/banco/bancosan/services/TransacoesContasService.java
@@ -1,13 +1,83 @@
 package com.banco.bancosan.services;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.banco.bancosan.enums.TipoTransacao;
+import com.banco.bancosan.models.TransacoesContasModel;
 import com.banco.bancosan.repositories.TransacoesContasRepository;
 
 @Service
 public class TransacoesContasService {
     
     @Autowired
-    private TransacoesContasRepository transacoesRepo;
+    private TransacoesContasRepository transacoesContasRepository;
+
+    @Transactional
+    public TransacoesContasModel createDeposito(TransacoesContasModel transacoesContasModel) {
+        transacoesContasModel.setTipo(TipoTransacao.DEPOSITO);
+        transacoesContasModel.setHorarioTransacao(LocalDateTime.now(ZoneId.of("UTC-3")));
+        transacoesContasModel.setSaldo(transacoesContasModel.getSaldo() + transacoesContasModel.getValor());
+        return transacoesContasRepository.save(transacoesContasModel);
+    }
+    @Transactional
+    public TransacoesContasModel createReceberTransferencia(TransacoesContasModel transacoesContasModel) {
+        transacoesContasModel.setTipo(TipoTransacao.RECEBER_TRANSFERENCIA);
+        transacoesContasModel.setHorarioTransacao(LocalDateTime.now(ZoneId.of("UTC-3")));
+        transacoesContasModel.setSaldo(transacoesContasModel.getSaldo() + transacoesContasModel.getValor());
+        return transacoesContasRepository.save(transacoesContasModel);
+    }
+    @Transactional
+    public TransacoesContasModel createReceberPix(TransacoesContasModel transacoesContasModel) {
+        transacoesContasModel.setTipo(TipoTransacao.RECEBER_PIX);
+        transacoesContasModel.setHorarioTransacao(LocalDateTime.now(ZoneId.of("UTC-3")));
+        transacoesContasModel.setSaldo(transacoesContasModel.getSaldo() + transacoesContasModel.getValor());
+        return transacoesContasRepository.save(transacoesContasModel);
+    }
+    @Transactional
+    public TransacoesContasModel createSaque(TransacoesContasModel transacoesContasModel) {
+        transacoesContasModel.setTipo(TipoTransacao.SAQUE);
+        transacoesContasModel.setHorarioTransacao(LocalDateTime.now(ZoneId.of("UTC-3")));
+        transacoesContasModel.setSaldo(transacoesContasModel.getSaldo() -
+         transacoesContasModel.getValor());
+        return transacoesContasRepository.save(transacoesContasModel);
+    }
+    @Transactional
+    public TransacoesContasModel createTransferencia(TransacoesContasModel transacoesContasModel) {
+        transacoesContasModel.setTipo(TipoTransacao.TRANSFERIR);
+        transacoesContasModel.setHorarioTransacao(LocalDateTime.now(ZoneId.of("UTC-3")));
+        transacoesContasModel.setSaldo(transacoesContasModel.getSaldo() -
+         transacoesContasModel.getValor());
+        return transacoesContasRepository.save(transacoesContasModel);
+    }
+    @Transactional
+    public TransacoesContasModel createEnviarPix(TransacoesContasModel transacoesContasModel) {
+        transacoesContasModel.setTipo(TipoTransacao.ENVIAR_PIX);
+        transacoesContasModel.setHorarioTransacao(LocalDateTime.now(ZoneId.of("UTC-3")));
+        transacoesContasModel.setSaldo(transacoesContasModel.getSaldo() -
+         transacoesContasModel.getValor());
+        return transacoesContasRepository.save(transacoesContasModel);
+    }
+    @Transactional
+    public TransacoesContasModel createPagamento(TransacoesContasModel transacoesContasModel) {
+        transacoesContasModel.setTipo(TipoTransacao.FAZER_PAGAMENTO);
+        transacoesContasModel.setHorarioTransacao(LocalDateTime.now(ZoneId.of("UTC-3")));
+        transacoesContasModel.setSaldo(transacoesContasModel.getSaldo() -
+         transacoesContasModel.getValor());
+        return transacoesContasRepository.save(transacoesContasModel);
+    }
+    public List<TransacoesContasModel> findAll() {
+        return transacoesContasRepository.findAll();
+    }
+
+    public Optional<TransacoesContasModel> findId(Integer id) {
+        return transacoesContasRepository.findById(id);
+    }
 }
