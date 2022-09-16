c3a1e3bc8148a3a8fb576c72f75096a3728da6b7
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-16 16:50:44 -0300

    criação de enum para tipo das operações no banco

---
+++ bancosan/src/main/java/com/banco/bancosan/enums/TipoTransacao.java
@@ -0,0 +1,11 @@
+package com.banco.bancosan.enums;
+
+public enum TipoTransacao {
+    SAQUE,
+    DEPOSITO,
+    RECEBER_TRANSFERENCIA,
+    TRANSFERIR,
+    ENVIAR_PIX,
+    RECEBER_PIX,
+    FAZER_PAGAMENTO
+}
