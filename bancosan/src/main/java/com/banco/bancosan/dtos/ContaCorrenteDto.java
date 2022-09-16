734cf1c04706067d2e7ab21384e78ed79bc1498c
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-16 00:13:57 -0300

    remorção de NotBlank e do construtor

--- bancosan/src/main/java/com/banco/bancosan/dtos/ContaCorrenteDto.java
+++ bancosan/src/main/java/com/banco/bancosan/dtos/ContaCorrenteDto.java
@@ -1,17 +1,12 @@
 package com.banco.bancosan.dtos;
 
-import javax.validation.constraints.NotBlank;
 
 import com.banco.bancosan.models.ClienteModel;
 
 public class ContaCorrenteDto {
     
-    @NotBlank
+    
     private ClienteModel cpf;
-
-    public ContaCorrenteDto(){
-        
-    }
 
     public ClienteModel getCpf() {
         return cpf;
