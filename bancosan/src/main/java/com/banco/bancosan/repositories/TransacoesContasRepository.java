3d5234f507e00146e227a3c87b8de5610f0b8aa9
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-19 23:58:49 -0300

    exclusão importes não utilizados

--- bancosan/src/main/java/com/banco/bancosan/repositories/TransacoesContasRepository.java
+++ bancosan/src/main/java/com/banco/bancosan/repositories/TransacoesContasRepository.java
@@ -1,10 +1,7 @@
 package com.banco.bancosan.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
 import com.banco.bancosan.models.TransacoesContasModel;
-import com.banco.bancosan.dtos.ContasSaldosDto;
 
 public interface TransacoesContasRepository extends JpaRepository<TransacoesContasModel, Integer>{
    
