11749816c0f771ab9de0c615f754f2ace88bb925
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-19 23:13:32 -0300

    realização de importes

--- bancosan/src/main/java/com/banco/bancosan/repositories/ContaCorrenteRepository.java
+++ bancosan/src/main/java/com/banco/bancosan/repositories/ContaCorrenteRepository.java
@@ -2,6 +2,8 @@ package com.banco.bancosan.repositories;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.banco.bancosan.models.ContaCorrenteModel;
 
