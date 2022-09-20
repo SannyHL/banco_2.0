de0a11bacf013936ae231e34c14028db3adba2fb
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-09-19 23:05:18 -0300

    Criação de query

--- bancosan/src/main/java/com/banco/bancosan/repositories/ContaCorrenteRepository.java
+++ bancosan/src/main/java/com/banco/bancosan/repositories/ContaCorrenteRepository.java
@@ -1,14 +1,13 @@
 package com.banco.bancosan.repositories;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import com.banco.bancosan.dtos.ContasSaldosDto;
 import com.banco.bancosan.models.ContaCorrenteModel;
-import com.banco.bancosan.models.TransacoesContasModel;
 
 public interface ContaCorrenteRepository extends JpaRepository<ContaCorrenteModel, Integer>{
+
+    @Query(value="SELECT c FROM ContaCorrenteModel c where c.numeroConta = :numero")
+    ContaCorrenteModel buscarNumeroConta(@Param("numero") Integer numero);
 
 }
